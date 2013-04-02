9404e70eb937343fa75c8acd9fe4862e60cd4b44
RaulMoyaReyes <raulmoyareyes@hotmail.com>
2013-04-02 20:22:25 +0200

    Creating the view for item and main.

--- public_html/backbonejs/js/main.js
+++ public_html/backbonejs/js/main.js
@@ -8,6 +8,8 @@
  */
 
 
+$(document).ready(function(){
+
 /* =================================================
  * MODEL
  */
@@ -15,8 +17,8 @@ var Item = Backbone.Model.extend({
     default: function() {
         return {
             id: cItems.nextId(),
-            name: anonymous,
-            comment: none
+            name: "anonymous",
+            comment: "none"
         };
     }
 });
@@ -44,62 +46,78 @@ var cItems = new CollectionItems;
 
 // vista para cada comentario
 var Comment = Backbone.View.extend({
-    
     tagName: "li",
-            
     // Asignamos el template a nuestra vista
     template: _.template($('#item-template').html()),
-            
     initialize: function() {
+        // Es un método de Underscore para guardar una referencia del
+        // objeto this en todos los métodos de esta clase.
+        _.bindAll(this);
+        
+        // Escucha el evento destroy del modelo de esta vista y llama al
+        // método remove de esta vista.
         this.listenTo(this.model, 'destroy', this.remove);
     },
-            
+    // asociamos cada evento de un selector a una función.
     events: {
-        "click .close" : "destroy"
+        "click .close": "clear"
     },
-            
     render: function() {
+        // Añado el html del template, al que hay que pasarle un objeto en 
+        // Json. Le paso el modelo que pertenece a esta vista.
         this.$el.html(this.template(this.model.toJSON()));
         return this;
     },
-    
-    destroy: function(){
-        
+    clear: function() {
+        // Destruyo el modelo de esta vista y por consiguiente la vista por 
+        // el evento que recojo arriba.
+        this.model.destroy();
     }
 });
 
 // vista para la aplicación general
 var AppView = Backbone.View.extend({
-    el: $(".list"),
+    el: $("#app"),
     // Asignamos el template a nuestra vista
-    template: _.template($('#app-template').html()),
+    statsTemplate: _.template($('#stats-template').html()),
     initialize: function() {
         // Es un método de Underscore para guardar una referencia del
         // objeto this en todos los métodos de esta clase.
         _.bindAll(this);
 
         // Manejadores para los eventos producidos por la colección
-        this.listenTo(cItems, 'add', this.add);
-        this.listenTo(cItems, 'destroy', this.remove);
-    },
-    events: {
-        // evento selector: método
-        "click .icon": "open" // es solo para mantener como se hace un evento
+        this.listenTo(cItems, 'add', this.added);
+        this.listenTo(cItems, 'all', this.render);
+        
+        this.footer = this.$("#footer");
+        //this.render();
     },
     render: function() {
+        this.footer.show();
+        this.footer.html(this.statsTemplate({nComments: "1"}));
         return this;
     },
-    remove: function() {
-
+    //removed: function() {
+    //
+    //},
+    // Al crear un nuevo elemento en cItems llama al metodo add y se crea una 
+    // vista para el comentario
+    added: function(item) {
+        var vComment = new Comment({model: item});
+        this.$("#comment-list").append(vComment.render().el);
     },
-    add: function() {
-
+    events: {
+        // añadir el evento para al hacer click crear un comentario
+    },
+    createComment: function(){
+        // aqui añadir un item a cItems
     }
 });
 
 /* =================================================
- * MAIN
+ * Objeto de la vista principal para comenzar la app
  */
-$(document).ready(function() {
-    var app = new AppView();
+
+var app = new AppView();
+
 });
