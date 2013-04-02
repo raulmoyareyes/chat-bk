fe4b308802e26646b723249f38f7cc304d8bf7b3
RaulMoyaReyes <raulmoyareyes@hotmail.com>
2013-04-02 18:33:03 +0200

    Create model and collection

--- public_html/backbonejs/js/main.js
+++ public_html/backbonejs/js/main.js
@@ -1,55 +1,105 @@
 /* 
-    Document   : js
-    Created on : 17-mar-2013, 19:18:46
-    Author     : Raúl Moya Reyes
-    Description:
-        Implementación en Javascript de la parte lógica 
-        utilizando Backbone (MVC)
-*/
+ Document   : js
+ Created on : 17-mar-2013, 19:18:46
+ Author     : Raúl Moya Reyes
+ Description:
+ Implementación en Javascript de la parte lógica 
+ utilizando Backbone (MVC)
+ */
 
 
 /* =================================================
  * MODEL
  */
 var Item = Backbone.Model.extend({
-  promptColor: function() {
-    var cssColor = prompt("Please enter a CSS color:");
-    this.set({color: cssColor});
-  }
+    default: function() {
+        return {
+            id: cItems.nextId(),
+            name: anonymous,
+            comment: none
+        };
+    }
 });
 
-/* =================================================
- * VIEW
- */
-var Comment = Backbone.View.extend({
-
-  tagName: "li",
-
-  className: "document-row",
-
-  events: {
-    "click .icon":          "open",
-    "click .button.edit":   "openEditDialog",
-    "click .button.delete": "destroy"
-  },
-
-  initialize: function() {
-    this.listenTo(this.model, "change", this.render);
-  },
-
-  render: function() {
-      
-  }
-
-});
 
 /* =================================================
  * COLLECTION
  */
-var Library = Backbone.Collection.extend({
-  model: Book
+var CollectionItems = Backbone.Collection.extend({
+    model: Item,
+    nextId: function() {
+        if (!this.length)
+            return 1;
+        return this.last().get('id') + 1;
+    },
+    comparator: 'id'
+});
+
+var cItems = new CollectionItems;
+
+
+/* =================================================
+ * VIEW 
+ */
+
+// vista para cada comentario
+var Comment = Backbone.View.extend({
+    
+    tagName: "li",
+            
+    // Asignamos el template a nuestra vista
+    template: _.template($('#item-template').html()),
+            
+    initialize: function() {
+        this.listenTo(this.model, 'destroy', this.remove);
+    },
+            
+    events: {
+        "click .close" : "destroy"
+    },
+            
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    },
+    
+    destroy: function(){
+        
+    }
+});
+
+// vista para la aplicación general
+var AppView = Backbone.View.extend({
+    el: $(".list"),
+    // Asignamos el template a nuestra vista
+    template: _.template($('#app-template').html()),
+    initialize: function() {
+        // Es un método de Underscore para guardar una referencia del
+        // objeto this en todos los métodos de esta clase.
+        _.bindAll(this);
+
+        // Manejadores para los eventos producidos por la colección
+        this.listenTo(cItems, 'add', this.add);
+        this.listenTo(cItems, 'destroy', this.remove);
+    },
+    events: {
+        // evento selector: método
+        "click .icon": "open" // es solo para mantener como se hace un evento
+    },
+    render: function() {
+        return this;
+    },
+    remove: function() {
+
+    },
+    add: function() {
+
+    }
 });
 
 /* =================================================
- * EVENTS
+ * MAIN
  */
+$(document).ready(function() {
+    var app = new AppView();
+});
