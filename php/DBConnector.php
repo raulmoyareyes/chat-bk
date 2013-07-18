6451fea450ac3b509ae911ff370b26c4aa49de99
Dev <dev@dev-laptop.(none)>
2013-07-18 20:57:06 +0200

    Servicio, clase de abstracción y DB funcionando.

--- php/DBConnector.php
+++ php/DBConnector.php
@@ -1,65 +1,94 @@
 <?php
 
+/*
+  Document   : php
+  Created on : 10-jul-2013, 19:18:46
+  Author     : Raúl Moya Reyes
+  Description:
+  Implementación de una capa de abstracción para la Base de Datos
+ */
+
+
+/* =============================================================================
+ * CLASE DE ABSTRACCIÓN PARA LA DB
+ * 
+ * Uso con Select
+ *      $query = "SELECT name, comment FROM cmchat WHERE id=?";
+ *      $data = array("i","{$id}");
+ *      $fields = array("Nombre" => "", "Comentario" => "");
+ *      $comments = DBConnector::execute($query, $data, $fields);
+ * 
+ * Uso con Insert
+ *      $query = "INSERT INTO 'cmchat' ('id','name','comment') VALUES(?,?,?)";
+ *      $data = array("iss", "{$id}","{$nombre}","{$comentario}");
+ *      $id_insert = DBConnector::execute($query, $data);
+ *      
+ */
+
 class DBConnector {
+
     protected static $conn;
     protected static $stmt;
     protected static $reflection;
     protected static $sql;
     protected static $data;
     protected static $results;
-    
-    protected static function connect(){
-        self::$conn = new mysqli("host", "user", "pass", "dbname");
+
+    protected static function connect() {
+        self::$conn = new mysqli("localhost", "user", "password", "pruebas");
     }
-    
+
     // Crea un statement a partir de $sql
-    protected static function prepare(){
+    protected static function prepare() {
         self::$stmt = self::$conn->prepare(self::$sql);
         self::$reflection = new ReflectionClass("mysqli_stmt");
     }
-    
+
     // Sustituye en $stmt las ? por los datos
-    protected static function setParams(){
+    protected static function setParams() {
         $method = self::$reflection->getMethod("bind_param");
         $method->invokeArgs(self::$stmt, self::$data);
     }
-    
+
     // Almacena los datos de la consulta en $results
-    protected static function getData($fields){
+    protected static function getData($fields) {
         $method = self::$reflection->getMethod("bind_result");
         $method->invokeArgs(self::$stmt, $fields);
-        while(self::$stmt->fetch()) {
+        while (self::$stmt->fetch()) {
             // Serializo y deserializo para hacer una copia de cada $fields
             self::$results[] = unserialize(serialize($fields));
         }
     }
-    
+
     // Cerrar la conexión a la DB
-    protected static function finish(){
+    protected static function finish() {
         self::$sql->close();
         self::$conn->close();
     }
-    
+
     // Ejecuta las sentencias SQL
-    public static function execute($sql, $data, $fields=FALSE){
+    public static function execute($sql, $data, $fields = FALSE) {
         self::$sql = $sql;
         self::$data = $data;
         self::connect();
         self::prepare();
-        self::setParams();
+        if (count(self::$data) > 1) {
+            self::setParams();
+        }
         self::$stmt->execute();
-        if($fields){
+        if ($fields) {
             self::getData($fields);
             // devolver results
-            // return 
+            return self::$results;
         } else {
-            if(strpos(self::$sql, strtoupper("INSERT")) === 0){
+            if (strpos(self::$sql, strtoupper("INSERT")) === 0) {
                 // devolver algo como true or false
-                // return self::$stmt->insert_id;
+                return self::$stmt->insert_id;
             }
         }
         self::finish();
     }
+
 }
 
 ?>
--- php/service.php
+++ php/service.php
@@ -1,28 +1,37 @@
 <?php
 
 include './DBConnector.php';
+//error_reporting(0);
 
-if (isset($_POST["id"])){
-    
-    $id = htmlspecialchars($_POST["id"]);
-    $nombre = htmlspecialchars($_POST["nombre"]);
-    $comentario = htmlspecialchars($_POST["comentario"]);
-    
+// cambiar a POST
+if (isset($_GET["id"])) {
+
+    $id = htmlspecialchars($_GET["id"]);
+    $nombre = htmlspecialchars($_GET["nombre"]);
+    $comentario = htmlspecialchars($_GET["comentario"]);
+
     insertarCometario($id, $nombre, $comentario);
-    
 } else {
-    devolverComentarios(0);
+    print(json_encode(devolverComentarios()));
 }
 
 function insertarCometario($id, $nombre, $comentario) {
-    
-    $query = "Insert ($id, $nombre, $comentario)";
 
+    $query = "INSERT INTO cmchat (id,name,comment) VALUES (?,?,?)";
+    $data = array("iss", "{$id}","{$nombre}","{$comentario}");
+    $id_insert = DBConnector::execute($query, $data);
+    
+    return $id_insert;
 }
 
-function devolverComentarios($time) {
+function devolverComentarios() {
 
-    return "Comentarios";
+    $query = "SELECT name, comment FROM cmchat";
+    $data = 0;
+    $fields = array("Nombre" => "", "Comentario" => "");
+    $comments = DBConnector::execute($query, $data, $fields);
+
+    return $comments;
 }
 
 ?>
