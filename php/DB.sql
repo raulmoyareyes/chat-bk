d78b95cdff454068fe6724caf414127ed226a663
Dev <dev@dev-laptop.(none)>
2013-07-21 18:30:09 +0200

    BD en sql

---
+++ php/DB.sql
@@ -0,0 +1,24 @@
+
+/* 
+ Document   : sql
+ Created on : 18-jul-2013, 19:18:46
+ Author     : Raúl Moya Reyes
+ Description:
+ Implementación de la Base de Datos
+ */
+
+
+/* =============================================================================
+ * Tabla para almacenar los comentarios en SQL
+ *
+ * CREATE USER user IDENTIFIED BY 'password';
+ * GRANT SELECT, INSERT ON *.* TO 'user'@'localhost';
+ */
+
+CREATE TABLE IF NOT EXISTS `cmchat` (
+  `id` int(10) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(20) DEFAULT 'Anonymous',
+  `comment` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`,`date`)
+)
